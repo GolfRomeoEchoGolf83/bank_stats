5487ff7b7059b67240de9ad438730213d53e9ba8
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-06-20 14:47:36 +0200

    adding some comments

--- Statistique.java
+++ Statistique.java
@@ -4,9 +4,11 @@ import java.util.*;
 public class Statistique {
 	public static void main (String [] arg)
 	{
+		// initialize variables
 		int nbCB = 0, nbCheque = 0, nbVirement = 0, nbDebit = 0;
 		float prctCB, prctCh, prctVi;
 		
+		// input from user
 		Scanner lectureClavier = new Scanner(System.in);
 		System.out.print("Nombre de paiements par Carte Bleue : ");
 		nbCB = lectureClavier.nextInt();
@@ -15,12 +17,15 @@ public class Statistique {
 		System.out.println("Nombre de virements automatiques : ");
 		nbVirement = lectureClavier.nextInt();
 		
+		// initialize nbDebit var which is the sum of all CB, Cheque and Virements done on the account
 		nbDebit = nbCB + nbCheque + nbVirement;
 		
+		// stats of CB, cheque and virement according to debit already done on the account
 		prctCB = (float) nbCB / nbDebit * 100;
 		prctCh = (float) nbCheque / nbDebit * 100;
 		prctVi = (float) nbVirement / nbDebit * 100;
 		
+		// output
 		System.out.println("Vous avez émis " + nbDebit + " ordres de debit");
 		System.out.print("dont " + prctCB + " % par Carte Bleue");
 		System.out.print("     " + prctCh + " % par chèque");
